ae39d7eb6b984939d73835696f988c0925531466
chenjie <chenjie.robbins@bytedance.com>
2018-03-28 11:26:18 +0800

    update apikey

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CuriousSamples = window.CuriousSamples || {};
 window.CuriousSamples.Config = window.CuriousSamples.Config || {
-  API_KEY: 'b9d57f00-31ad-11e8-89db-a948a24e5587'
+  API_KEY: '98e2f180-3237-11e8-9306-135ccc79482a'
 };
